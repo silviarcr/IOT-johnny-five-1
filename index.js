496f72fb1186e86462cfb514dd522782cc47f3e6
SILVIA REGINA COELHO DA ROSA <44080247249@jpmorgan.sp>
2018-01-16 21:16:42 -0200

    projeto botao servo led portao fechado aberto

--- index.js
+++ index.js
@@ -9,8 +9,41 @@ const arduino = new five.Board({
 
 function callback() {
     //chamada pra executar alguma ação
-    const led = new five.Led(13);
-    led.blink(500);
+    //const led = new five.Led(13);
+    // led.blink(500);
+    var led = new five.Led(8);
+    var botao = new five.Button(2);
+    var servo = new five.Servo({
+        pin: 10,
+        range: [10, 90],
+        startAt: 19
+    });
+
+
+    var portaoAberto = false;
+
+    botao.on("down", function () {
+        if (!portaoAberto) {
+            console.log("botao pressionado");
+            servo.to(90, 3 * 1000);
+            led.on();
+            portaoAberto = true;
+        }
+        else {
+            servo.to(19, 1 * 1000);
+            led.off();
+            portaoAberto = false;
+        }
+    });
+
+    //     botao.on("up", function () {
+    //     console.log("botao released");
+    //     servo.to(19, 1 * 1000);
+    //     led.off();
+    //});
+
+
 }
 
 arduino.on("ready", callback);
+
